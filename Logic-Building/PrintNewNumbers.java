e5eee8e1c3b8af0f307c284410c2d46d5aba1970
tahaseen06 <taha12shaik@gmail.com>
2026-07-07 22:31:19 +0530

    Add files via upload

---
+++ Logic-Building/PrintNewNumbers.java
@@ -0,0 +1,12 @@
+package logicbuilding;
+
+public class PrintNewNumbers {
+	public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+
+    }
+
+}
