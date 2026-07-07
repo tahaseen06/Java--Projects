b51988eb537193e702ed44045f95129fe4147d6a
tahaseen06 <taha12shaik@gmail.com>
2026-07-07 22:49:38 +0530

    Add files via upload

---
+++ Logic-Building/ReverseNumbers.java
@@ -0,0 +1,10 @@
+package logicbuilding;
+
+public class ReverseNumbers {
+	public static void main(String[] args) {
+		for (int i = 100; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
