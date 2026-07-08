a1e22e80c7dced2bad452de2771b5bb3c677b591
tahaseen06 <taha12shaik@gmail.com>
2026-07-08 22:46:01 +0530

    Add files via upload

---
+++ OOPS/Bird.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+public class Bird extends Animal {
+
+    @Override
+    public void eat() {
+        System.out.println("Bird is eating");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Bird is sleeping");
+    }
+
+    public void fly() {
+        System.out.println("Bird is flying");
+    }
+}
