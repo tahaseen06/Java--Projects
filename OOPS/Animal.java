d796f3f98443b9d7bab25b45a195fd3f71ef533a
tahaseen06 <taha12shaik@gmail.com>
2026-07-08 22:45:20 +0530

    Add files via upload

---
+++ OOPS/Animal.java
@@ -0,0 +1,11 @@
+package OOPS;
+
+public class Animal {
+	 public void eat() {
+	        System.out.println("Animal is eating");
+	    }
+
+	    public void sleep() {
+	        System.out.println("Animal is sleeping");
+	    }
+}
