2d964a1450e358f1499a6c8c67f3d25dab8cc711
tahaseen06 <taha12shaik@gmail.com>
2026-07-08 21:53:53 +0530

    Add files via upload

---
+++ OOPS/Calculator.java
@@ -0,0 +1,18 @@
+package OOPS;
+
+public class Calculator {
+    public static int powerInt(int num1, int num2) {
+        return (int)Math.pow(num1, num2);
+    }
+
+    // Static method for double
+    public static double powerDouble(double num1, int num2) {
+        return Math.pow(num1, num2);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("powerInt = " + powerInt(2, 3));
+        System.out.println("powerDouble = " + powerDouble(2.5, 2));
+    }
+}
