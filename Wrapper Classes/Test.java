65a207a240b02044a7f59130b6a08d00b3090ce8
tahaseen06 <taha12shaik@gmail.com>
2026-07-14 22:28:46 +0530

    Add files via upload

---
+++ Wrapper Classes/Test.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Please provide a number.");
+            return;
+        }
+
+        int n = Integer.parseInt(args[0]);
+
+        System.out.println("Given Number :" + n);
+        System.out.println("Binary equivalent :" + Integer.toBinaryString(n));
+        System.out.println("Octal equivalent :" + Integer.toOctalString(n));
+        System.out.println("Hexadecimal equivalent :" + Integer.toHexString(n).toUpperCase());
+    }
+}
