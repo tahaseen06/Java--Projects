90de39f19601a4bbb08d024aca2774b981e89195
tahaseen06 <taha12shaik@gmail.com>
2026-07-15 15:39:28 +0530

    Add files via upload

---
+++ Collections/Project1/Employee.java
@@ -0,0 +1,32 @@
+package employee;
+
+public class Employee implements Comparable<Employee> {
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private String email;
+    private String address;
+
+    public Employee(String firstName, String lastName, String mobile, String email, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mobile = mobile;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public int compareTo(Employee e) {
+        return this.firstName.compareToIgnoreCase(e.firstName);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s %-15s %-15s %-30s %-15s",
+                firstName, lastName, mobile, email, address);
+    }
+}
