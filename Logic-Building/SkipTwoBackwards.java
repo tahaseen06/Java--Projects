219b4bda6b8a8d2b4694e8994cce322af9f8c31a
tahaseen06 <taha12shaik@gmail.com>
2026-07-16 18:27:04 +0530

    Add files via upload

---
+++ Logic-Building/SkipTwoBackwards.java
@@ -0,0 +1,9 @@
+package logicbuilding;
+
+public class SkipTwoBackwards {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 1; i -= 3) {
+            System.out.print(i + " ");
+        }
+    }
+}
